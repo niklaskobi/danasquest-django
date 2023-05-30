82ae71f4650358912a429cce5d58f46bd29244ff
niklaskobi <niklas.kobelev@gmail.com>
2023-05-30 10:16:27 +0200

    style: remove default menu add 2 buttons

--- danasquest/arcweave/views.py
+++ danasquest/arcweave/views.py
@@ -6,10 +6,11 @@ def view_import(request):
     payload = {
         'title': 'Import',
         'urls': {
-            'background': '../../static/arcweave/danas-quest-2023-05-17-130459/assets/Locations/Episode_1/kitchen.png',
-            'char_left': '../../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Dana.png',
-            'char_right': '../../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Marta.png',
+            'background': '../static/arcweave/danas-quest-2023-05-17-130459/assets/Locations/Episode_1/kitchen.png',
+            'char_left': '../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Dana.png',
+            'char_right': '../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Marta.png',
         }
     }
 
-    return render(request, 'arcweave/page.html', payload)
+    # return render(request, 'arcweave/page.html', payload)
+    return render(request, 'base.html', payload)
