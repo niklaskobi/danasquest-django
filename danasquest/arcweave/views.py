9158abdb1fca410ee09eae515cb009e377c17efa
niklaskobi <niklas.kobelev@gmail.com>
2023-06-10 11:41:07 +0200

    feat: add basic audio functionality

--- danasquest/arcweave/views.py
+++ danasquest/arcweave/views.py
@@ -20,14 +20,21 @@ def get_element_by_id(data, id):
     return data["elements"][id]
 
 
-def get_cover_url_from_element(element, project):
+def get_cover_asset_from_element(element, project):
     id = element["assets"]["cover"]["id"]
-    return get_asset_by_id(project, id).file
+    return get_asset_by_id(project, id)
 
 
 def get_asset_by_id(project, id):
     asset_name = project.json["assets"][id]["name"]
     return AssetFile.objects.get(project=project, name=asset_name)
+
+
+def get_audio_assets_from_element(element, project):
+    audio_assets = []
+    for audio in element["assets"]["audio"]:
+        audio_assets.append(get_asset_by_id(project, audio['asset']))
+    return audio_assets
 
 
 # Create your views here.
@@ -36,11 +43,14 @@ def view_import(request):
     root_board = get_board_by_name(project.json, "Root")
     start_board = get_board_by_id(project.json, root_board["children"][0])
     starting_element = get_starting_element(project.json)
-    cover = get_cover_url_from_element(starting_element, project)
+    cover = get_cover_asset_from_element(starting_element, project)
+    audio_list = get_audio_assets_from_element(starting_element, project)
     payload = {
         'title': 'Import',
+        'content': starting_element["content"],
         'assets': {
             'background': cover,
+            'audio': audio_list,
             'char_left': '../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Dana.png',
             'char_right': '../static/arcweave/danas-quest-2023-05-17-130459/assets/Characters/original/Marta.png',
         }
